f7f11b82c5c17a9886b087a344cde0ca29063934
Alexander Page <alexander.page@warriorlife.net>
2018-11-29 09:31:52 -0800

    i love you

---
+++ scripts/conversation.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function hello()
+{
+	alert('THE POWER OF HEMAN')
+}
+
+function HEMAN()
+{
+	alert('AYAYAYYAYAYAYAYAYAYAYYAYAYAYAYYAYYAYAYYAYAAYYAYAYYAYAYYAYYAYYAYAYAYYAAYYAYAYAYYAYAYYYYAYAYAYAYAYAYAYAYY')
+}
+
+function cat()
+{
+	alert('ITS A GIANT TIGER')
+}
+
+function skelitor()
+{
+	alert('NYA NYA NYA')
+}
